1c2e03eb56b14ed26cfb14304a37a6cf745f4607
Christopher Fox <cfox1290@gmail.com>
2018-05-20 11:01:08 -0600

    finished basic countdown app

--- src/app.js
+++ src/app.js
@@ -1,21 +1,44 @@
 import React from "react";
 import "./app.css";
+import Clock from "./clock";
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            deadline: "February 21, 2019",
+            newDeadline: ""
+        }
+        this.changeDeadline = this.changeDeadline.bind(this)
+        this.handleInputChange = this.handleInputChange.bind(this)
+    }
+
+    changeDeadline() {
+        this.setState({
+            deadline: this.state.newDeadline,
+            newDeadline: ""
+        })
+    }
+
+    handleInputChange(e) {
+        this.setState({
+            newDeadline: e.target.value
+        })
+    }
+
 
     render() {
         return (
-            <div classsName="App">
-                <div className="App-title">Countdown to February 21, 2019</div>
+            <div className="App">
+                <div className="App-title">Countdown to {this.state.deadline}</div>
+                <Clock 
+                    deadline={this.state.deadline}
+                />
                 <div>
-                    <div className="Clock-days">14 Days</div>
-                    <div className="Clock-hours">30 hours</div>
-                    <div className="Clock-minutes">30 minutes</div>
-                    <div className="Clock-seconds">20 seconds</div>
-                </div>
-                <div>
-                    <input placeholder="new date" />
-                    <button>Sumbmit</button>
+                    <input
+                        onChange={event => this.handleInputChange(event)}
+                        placeholder="new date" />
+                    <button onClick={() => this.changeDeadline()}>Submit</button>
                 </div>
             </div>
         )
